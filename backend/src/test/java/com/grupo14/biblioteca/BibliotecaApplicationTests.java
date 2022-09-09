a841c59b3e3012e55e8aa6c031436910bf4ce7a7
Mily <milyalvarez.26@gmail.com>
2022-09-08 22:01:54 -0300

    initial commit

---
+++ backend/src/test/java/com/grupo14/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo14.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
